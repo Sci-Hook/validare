7a729aae94779611d23113f67f68d12afad153ba
lim10tech <lim10tech@gmail.com>
2024-07-02 10:19:17 +0300

    fix: type error

--- src/random-number/index.ts
+++ src/random-number/index.ts
@@ -1,8 +1,20 @@
-export function randomize(options:{min?:number,max?:number,len?:number}) :number {
+export function randomize(options:{min?:number|string,max?:number|string,len?:number|string}) : number|void {
     
+    if (typeof options.min != 'number') {
+        return console.error('To use this feature, the min_length property must take a number type value.')
+    }
+
+    if (typeof options.max != 'number') {
+        return console.error('To use this feature, the max_length property must take a number type value')
+    }
+
+    if (typeof options.len != 'number') {
+        return console.error('To use this feature, the length property must take a number type value')
+    }
+
+
     var min:number = 0;
     var max:number = 1;
-    var b:number = 10;
 
     if (!options.len) {
         if (options.min) min = options.min
