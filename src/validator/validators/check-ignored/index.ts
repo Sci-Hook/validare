f8abb7cda15232ebfafc20d98b5282d3cc7d30c9
lim10tech <lim10tech@gmail.com>
2024-07-31 20:11:17 +0300

    fix: add validate ignored not used resolve

--- src/validator/validators/check-ignored/index.ts
+++ src/validator/validators/check-ignored/index.ts
@@ -3,10 +3,8 @@ import { string_schemas } from "../../../types/schemas";
 export function validate_igonered(schema:string_schemas,value:string) {
     return new Promise<'ignored'|'no_error'>(async (resolve, reject) => {
 
-        if (schema.ignored?.indexOf(value) != -1) {
-            return resolve('ignored');
-        }
-
+        if (!schema.ignored) return resolve('no_error')
+        if (schema.ignored?.indexOf(value) != -1) return resolve('ignored');
         return resolve('no_error')
         
     });
