c8c276f0cecb43c4fcf77e7367bbdb7e1af8e32c
Emirhan Gerçeker <emirhan@scihook.org>
2024-11-14 09:45:13 +0300

    build(webpack): compiled webpack

--- test.ts
+++ test.ts
@@ -1,13 +1,13 @@
 import * as express from 'express';
 import {Schema, loadSchemas, validateSwitch, validator, validateFields} from './index';
 
-var test = new Schema({
-    type:'url',
-    ignored_hostnames:['bilimetri.com']
-});
+// var test = new Schema({
+//     type:'url',
+//     ignored_hostnames:['bilimetri.com']
+// });
 
-async function main() {
-    console.log(await test.validate('http://bilimetri.com'))
-}
+// async function main() {
+//     console.log(await test.validate('http://bilimetri.com'))
+// }
 
-main();
+// main();
